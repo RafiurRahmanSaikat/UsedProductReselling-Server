e507092025f8d7f75e6932bdca9456120a61afc3
unknown <md.rafiur.rahman.saikat@gmail.com>
2022-11-26 19:07:05 +0600

    JWT added

--- index.js
+++ index.js
@@ -27,29 +27,36 @@ const ALLUSER = client.db("DreamBike").collection("Users");
 const BIKE = client.db("DreamBike").collection("Bikes");
 const BOOKED = client.db("DreamBike").collection("Booked");
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//       return res.status(401).send({ message: "unauthorized access" });
-//     }
-//     const token = authHeader.split(" ")[1];
 
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//       if (err) {
-//         return res.status(403).send({ message: "Forbidden access" });
-//       }
-//       req.decoded = decoded;
-//       next();
-//     });
-//   }
-//   app.post("/jwt", (req, res) => {
-//     const user = req.body;
-//     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-//       expiresIn: "1d",
-//     });
-//     res.send({ token });
-//   });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+
+
+  app.post("/jwt", (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+    res.send({ token });
+  });
+
+  
 app.post("/adduser", async (req, res) => {
   try {
     const result = await ALLUSER.insertOne(req.body);
